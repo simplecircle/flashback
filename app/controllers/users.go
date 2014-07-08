71f944e87ecd042d98d3a14f9376c6426652e76a
Elliott Golden <studio@simplecircle.net>
2014-07-07 21:49:01 -0400

    get CurrentUser from auth cookie

--- app/controllers/users.go
+++ app/controllers/users.go
@@ -6,10 +6,10 @@ import(
   "labix.org/v2/mgo"
   "labix.org/v2/mgo/bson"
   "code.google.com/p/go.crypto/bcrypt"
-  "reflect"
   "github.com/dchest/uniuri"
   "net/http"
-  //"time"
+  //"reflect"
+  //"strings"
 )
 
 type Users struct {
@@ -24,9 +24,26 @@ type User struct {
 
 
 func (c Users) New() revel.Result {
-	return c.Render()
+  return c.Render()
 }
 
+func (c Users) CurrentUser() User {
+  session, err := mgo.Dial("mongodb://elliottg:monkey75@kahana.mongohq.com:10026/flashbackDev")
+  if err != nil {
+          panic(err)
+  }
+  defer session.Close()
+  cookie, _ := c.Request.Cookie("authToken")
+  cookieAuthToken := cookie.Value
+  currentUser := User{}
+  coll := session.DB("flashbackDev").C("users")
+  coll.Find(bson.M{"authtoken" : cookieAuthToken}).One(&currentUser)
+  if err != nil {
+          panic(err)
+  }
+  //fmt.Println(currentUser.Email)
+  return currentUser
+}
 func (c Users) Create(email, password string) revel.Result {
   session, err := mgo.Dial("mongodb://elliottg:monkey75@kahana.mongohq.com:10026/flashbackDev")
   if err != nil {
@@ -38,7 +55,6 @@ func (c Users) Create(email, password string) revel.Result {
   coll := session.DB("flashbackDev").C("users")
   bcryptPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
   authToken := uniuri.NewLen(22)
-  fmt.Println(authToken)
   user := &User{Id: bson.NewObjectId(), Email: email, Password: bcryptPassword, AuthToken: authToken}
   err = coll.Insert(user)
   if err != nil {
@@ -50,26 +66,27 @@ func (c Users) Create(email, password string) revel.Result {
 		Value:    authToken,
 		Path:     "/",
 	})
+  //fmt.Println(http.ReadSetCookies(c.Request.Header))
 
-  fmt.Println(bcryptPassword)
-  fmt.Println(user.Password)
-  fmt.Println(reflect.TypeOf(user.Password).Kind())
-  fmt.Println(reflect.TypeOf(bcryptPassword).Kind())
+  //fmt.Println(bcryptPassword)
+  //fmt.Println(user.Password)
+  //fmt.Println(reflect.TypeOf(user.Password).Kind())
+  //fmt.Println(reflect.TypeOf(bcryptPassword).Kind())
   err = bcrypt.CompareHashAndPassword(user.Password, []byte(password))
    if err != nil {
       panic(err)
   }
-
   var updatedUser User
   coll.FindId(user.Id).One(&updatedUser)
-  fmt.Println(updatedUser.Password)
-  //fmt.Println(user.Id)
-  revel.TRACE.Printf(email)
-  revel.TRACE.Printf(password)
+  //fmt.Println(updatedUser.Password)
+  ////fmt.Println(user.Id)
+  //revel.TRACE.Printf(email)
+  //revel.TRACE.Printf(password)
   if err != nil {
           panic(err)
   }
 
 
+  fmt.Println(c.CurrentUser().Email)
 	return c.Redirect(Cards.Index)
 }
